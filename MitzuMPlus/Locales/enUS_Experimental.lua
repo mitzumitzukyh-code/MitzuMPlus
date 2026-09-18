95f3a7ad5c922f44c4b4a833edf727d1467cde0b
Mitzukyhs <mitzumitzukyh@gmail.com>
2026-09-18 00:59:17 -0400

    feat(lab): add English LFG composition locale

---
+++ MitzuMPlus/Locales/enUS_Experimental.lua
@@ -0,0 +1,9 @@
+-- Experimental Mythic+ lab localization. English is the base/default locale.
+local L = LibStub("AceLocale-3.0"):NewLocale("MitzuMPlus", "enUS", true)
+if not L then return end
+
+L["LFG_NEEDS_PREFIX"] = "Party needs"
+L["LFG_COVERAGE_READY"] = "Core coverage ready"
+L["LFG_NEED_LUST"] = "Bloodlust"
+L["LFG_NEED_BREZ"] = "Battle rez"
+L["LFG_NEED_DPS_N"] = "DPS x%d"
