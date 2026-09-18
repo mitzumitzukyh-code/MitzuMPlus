7de7ffb730529571227afd390f15103ac7ecf988
Mitzukyhs <mitzumitzukyh@gmail.com>
2026-09-18 01:01:32 -0400

    fix(lab): isolate experimental Spanish locale namespace

--- MitzuMPlus/Locales/esES_Experimental.lua
+++ MitzuMPlus/Locales/esES_Experimental.lua
@@ -1,9 +1,11 @@
 -- Localizacion del laboratorio experimental Mythic+.
-local L = LibStub("AceLocale-3.0"):NewLocale("MitzuMPlus", "esES")
+local L = LibStub("AceLocale-3.0"):NewLocale("MitzuMPlusExperimental", "esES")
 if not L then return end
 
 L["LFG_NEEDS_PREFIX"] = "Falta en el grupo"
 L["LFG_COVERAGE_READY"] = "Cobertura basica lista"
+L["LFG_NEED_TANK"] = "Tanque"
+L["LFG_NEED_HEALER"] = "Sanador"
 L["LFG_NEED_LUST"] = "Heroismo"
 L["LFG_NEED_BREZ"] = "Resurreccion en combate"
 L["LFG_NEED_DPS_N"] = "DPS x%d"
