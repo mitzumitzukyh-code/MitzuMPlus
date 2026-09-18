4cb447d482d7f144d3801be4fa739ffc46e5169b
Mitzukyhs <mitzumitzukyh@gmail.com>
2026-09-18 00:59:23 -0400

    feat(lab): add Spanish LFG composition locale

---
+++ MitzuMPlus/Locales/esES_Experimental.lua
@@ -0,0 +1,9 @@
+-- Localizacion del laboratorio experimental Mythic+.
+local L = LibStub("AceLocale-3.0"):NewLocale("MitzuMPlus", "esES")
+if not L then return end
+
+L["LFG_NEEDS_PREFIX"] = "Falta en el grupo"
+L["LFG_COVERAGE_READY"] = "Cobertura basica lista"
+L["LFG_NEED_LUST"] = "Heroismo"
+L["LFG_NEED_BREZ"] = "Resurreccion en combate"
+L["LFG_NEED_DPS_N"] = "DPS x%d"
